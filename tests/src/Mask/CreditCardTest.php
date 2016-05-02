e6cfa23b85f79165c6f9ac71093a7e37d9116ca5
Mariano F.co Benítez Mulet <nanodevel@gmail.com>
2016-05-02 19:46:10 +0200

    Fix long array in 5.5

--- tests/src/Mask/CreditCardTest.php
+++ tests/src/Mask/CreditCardTest.php
@@ -20,14 +20,12 @@ class CreditCardTest extends \PHPUnit_Framework_TestCase
      * @var array Strings containing valid credit cards
      */
     protected $validCcs = [
-        'My credit card is 4556168690125914. Please, spread it!'
-        => 'My credit card is ************5914. Please, spread it!',
-        'My credit cards are 6011 3885 3731 4927 and 5465-7136-4763-2236. Buy something!'
-        => 'My credit cards are ************4927 and ************2236. Buy something!',
-        'Creditcard rampage! 6011 612890653518, 601167 7315389477, '
-        . '60117 4607031 4770, 6011-3885373-14927, 6011-056532 328271'
-        => 'Creditcard rampage! ************3518, ************9477, '
-        . '************4770, ************4927, ************8271'
+        'My credit card is 4556168690125914. Please, spread it!' =>
+        'My credit card is ************5914. Please, spread it!',
+        'My credit cards are 6011 3885 3731 4927 and 5465-7136-4763-2236. Buy something!' =>
+        'My credit cards are ************4927 and ************2236. Buy something!',
+        'Creditcard rampage! 6011 612890653518, 601167 7315389477, 60117 4607031 4770, 6011-3885373-14927' =>
+        'Creditcard rampage! ************3518, ************9477, ************4770, ************4927'
     ];
 
     /**
